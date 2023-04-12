a0dd55512d73d1ce8845c0ea638992fa2aa3cb55
EnzoFoulon <enzo_foulon@icloud.com>
2023-04-12 10:54:08 +0200

    ajout du fichier a vérifier

---
+++ code.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MonNamespace;
+
+/**
+ * Nom du fichier: code.php
+ *
+ * Classe Validateur : permet de valider une chaîne de caractères
+ *
+ * Cette classe permet de valider une chaîne de caractères en vérifiant si elle est d'au moins deux caractères.
+ */
+class Validateur
+{
+    /**
+     * Vérifie si une chaîne de caractères est d'au moins deux caractères de longueur.
+     *
+     * @param string $input La chaîne de caractères à vérifier.
+     *
+     * @return bool Renvoie true si la chaîne est d'au moins deux caractères de longueur, false sinon.
+     */
+    public function check($input)
+    {
+        if (strlen($input) < 2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+// End of file (EOF).
